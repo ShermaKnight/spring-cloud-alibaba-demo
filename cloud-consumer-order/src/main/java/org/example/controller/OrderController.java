a6fa42e02081fcceb98422f6f91b22e42d0bfcc7
vmartinir <vmartinir@aliyun.com>
2021-10-01 10:10:59 +0800

    Add cloud consumer with nacos

--- cloud-consumer-order/src/main/java/org/example/controller/OrderController.java
+++ cloud-consumer-order/src/main/java/org/example/controller/OrderController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Optional;
